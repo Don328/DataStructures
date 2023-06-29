25d8e52e26bb00068fc26091cf382b2c2e507370
Don <donavan.olsen@gmail.com>
2023-06-29 11:32:36 -0700

    Refactoring Queue object 'Que'

--- src/Que/Que.cs
+++ src/Que/Que.cs
@@ -1,50 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Que;
 
-namespace Que;
 public class Que<T> where T : class
 {
     private T[] _items = default!;
-    private int _head;
+    private int _cursor;
     private int _tail;
 
-    public Que() { }
-    public Que(T item) { }
-    public Que(IEnumerable<T> items) { }
+    public Que() 
+    {
+        GetEmptyQue();
+    }
+    public Que(T item) 
+    {
+        _items = new T[1];
+        _cursor = 0;
+        _tail = 0;
+    }
+    public Que(IEnumerable<T> items) 
+    {
+        _cursor = 0;
+        _tail = -1;
+        var count = items.Count();
+        _items = new T[count];
+
+        foreach (var item in items)
+        {
+            _items[++_tail] = item;
+        }
+    }
 
     public void Push(T item)
     {
-        var newCount = _tail - _head + 2;
+        if (_cursor == -1)
+        {
+            _items = new T[1];
+            _items[0] = item;
+            _cursor = 0;
+            _tail = 0;
+            return;
+        }
+
+        var newCount = _tail - _cursor + 2;
         var newTail = newCount - 1;
         var newItems = new T[newCount];
         newItems[newTail] = item;
-        
-        for (int i = _head; i < _tail; i++)
+
+        for (int i = _cursor; i <= _tail; i++)
         {
-            newItems[i - _head] = _items[i];
+            var newIndex = (i - _cursor);
+            newItems[newIndex] = _items[i];
         }
 
         _items = newItems;
-        _head = 0;
+        _cursor = 0;
         _tail = newTail;
     }
 
     public T Peek()
     {
-        if (_head > _tail) return default!;
+        if (_cursor > _tail) return default!;
         
-        return _items[_head];
+        return _items[_cursor];
     }
 
     public T Pop()
     {
-        if (_head > _tail) return default!;
+        if (_cursor > _tail) return default!;
         
-        var value = _items[_head];
-        _head++;
+        var value = _items[_cursor];
+        _cursor++;
+
+        if (_cursor > _tail)
+        {
+            GetEmptyQue();
+        }
+        
         return value;
+    }
+
+    private void GetEmptyQue()
+    {
+        _items = Array.Empty<T>();
+        _cursor = -1;
+        _tail = -1;
     }
 }
